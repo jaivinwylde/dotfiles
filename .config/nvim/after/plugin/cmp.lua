38f8e024da6c130caef41aac09eeecb3bbc55d3b
Jaivin Wylde <me@jai.vin>
2024-03-07 19:24:09 -0800

    minor stuff

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -3,61 +3,61 @@ local lspkind = require("lspkind")
 local snip = require("luasnip")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			snip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<c-u>"] = cmp.mapping.scroll_docs(-4),
-		["<c-d>"] = cmp.mapping.scroll_docs(4),
-		["<c-space>"] = cmp.mapping.complete(),
-		["<c-j>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif snip.expand_or_jumpable() then
-				snip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<s-tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif snip.jumpable(-1) then
-				snip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" },
-		{ name = "buffer", keyword_length = 5 },
-	},
-	experimental = {
-		ghost_text = true,
-	},
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = "sympol_text",
-			menu = {
-				nvim_lsp = "[lsp]",
-				luasnip = "[snip]",
-				path = "[path]",
-				buffer = "[buf]",
-			},
-		}),
-	},
-	window = {
-		documentation = cmp.config.window.bordered(),
-	},
+  snippet = {
+    expand = function(args)
+      snip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<c-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<c-d>"] = cmp.mapping.scroll_docs(4),
+    ["<c-space>"] = cmp.mapping.complete(),
+    ["<c-y>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif snip.expand_or_jumpable() then
+        snip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<s-tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif snip.jumpable(-1) then
+        snip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = "buffer",  keyword_length = 5 },
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "sympol_text",
+      menu = {
+        nvim_lsp = "[lsp]",
+        luasnip = "[snip]",
+        path = "[path]",
+        buffer = "[buf]",
+      },
+    }),
+  },
+  window = {
+    documentation = cmp.config.window.bordered(),
+  },
 })
 
 -- Setup autopairs
